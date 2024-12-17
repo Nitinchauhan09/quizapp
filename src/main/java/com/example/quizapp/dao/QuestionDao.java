049e1821a4d26dfd1ad3fbb9848c32d58681d50e
nitinchauhan09 <thisisnitinchauhan@gmail.com>
2024-12-17 09:45:52 +0530

    commit changes

--- src/main/java/com/example/quizapp/dao/QuestionDao.java
+++ src/main/java/com/example/quizapp/dao/QuestionDao.java
@@ -1,6 +1,6 @@
 package com.example.quizapp.dao;
 
-import com.example.quizapp.Question;
+import com.example.quizapp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,10 +11,13 @@ import java.util.List;
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer> {
 
+    // Not working because of case sensitivity
+    // public List<Question> findByCategory(String category);
+
     @Query(value = "select * from question where LOWER(category) = LOWER(:category)", nativeQuery = true)
     public List<Question> findByCategory(@Param("category") String category);
 
+    @Query(value = "SELECT * FROM question q WHERE q.category = :category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionsByCategory(String category, int numQ);
 
-//    Not working because of case sensitivity
-//    public List<Question> findByCategory(String category);
 }
