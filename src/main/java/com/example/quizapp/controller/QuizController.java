d54b27096d9058078a0ab24bc8595d2a0df0b129
nitinchauhan09 <thisisnitinchauhan@gmail.com>
2024-12-17 10:54:29 +0530

    Added Score Functionality

--- src/main/java/com/example/quizapp/controller/QuizController.java
+++ src/main/java/com/example/quizapp/controller/QuizController.java
@@ -1,11 +1,9 @@
 package com.example.quizapp.controller;
 
-import com.example.quizapp.model.Question;
 import com.example.quizapp.model.QuestionWrapper;
 import com.example.quizapp.model.Response;
 import com.example.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
